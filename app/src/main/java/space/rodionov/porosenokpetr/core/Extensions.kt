3f64367d1f457ea740bb16400010f6a6c46479d2
Aleksej <rodionovjr91@gmail.com>
2022-03-08 19:47:35 +0300

    Importinf workmanager stuff and drawable and so on

--- app/src/main/java/space/rodionov/porosenokpetr/core/Extensions.kt
+++ app/src/main/java/space/rodionov/porosenokpetr/core/Extensions.kt
@@ -1,8 +1,11 @@
 package space.rodionov.porosenokpetr.core
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.inputmethod.InputMethodManager
 import android.widget.AutoCompleteTextView
+import androidx.core.content.ContextCompat
 import java.math.BigDecimal
 
 fun Float.roundToTwoDecimals(): Float {
@@ -15,4 +18,15 @@ fun AutoCompleteTextView.showKeyboard() {
     this.requestFocus()
     val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun Context.vectorToBitmap(drawableId: Int): Bitmap? {
+    val drawable = ContextCompat.getDrawable(this, drawableId) ?: return null
+    val bitmap = Bitmap.createBitmap(
+        drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+    ) ?: return null
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
 }
