2c245f28a73c48e24ad94c9566c1a8000ee5a30a
Aleksej <rodionovjr91@gmail.com>
2023-08-06 04:21:02 +0600

    flavors done

--- app/src/swedishdriller/java/space/rodionov/porosenokpetr/core/domain/use_case/GetLearnedLanguageUseCase.kt
+++ app/src/swedishdriller/java/space/rodionov/porosenokpetr/core/domain/use_case/GetLearnedLanguageUseCase.kt
@@ -1,6 +1,7 @@
 package space.rodionov.porosenokpetr.core.domain.use_case
 
 import space.rodionov.porosenokpetr.core.util.Language
+import space.rodionov.porosenokpetr.core.util.Language.Companion.LANGUAGE_SE
 
 class GetLearnedLanguageUseCase {
     operator fun invoke() = Language.resolveLanguage(LANGUAGE_SE)
