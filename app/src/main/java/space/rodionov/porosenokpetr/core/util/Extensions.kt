9377de0ffdec30df5559a12cbaf3a69b5021ef8c
Aleksej <rodionovjr91@gmail.com>
2023-09-03 13:08:21 +0600

    adapter refactored

--- app/src/main/java/space/rodionov/porosenokpetr/core/util/Extensions.kt
+++ app/src/main/java/space/rodionov/porosenokpetr/core/util/Extensions.kt
@@ -2,7 +2,6 @@ package space.rodionov.porosenokpetr.core.util
 
 import android.app.Activity
 import android.content.Intent
-import android.util.Log
 import space.rodionov.porosenokpetr.core.domain.model.Word
 import space.rodionov.porosenokpetr.core.util.Constants.WORD_ACTIVE
 import space.rodionov.porosenokpetr.core.util.Constants.WORD_LEARNED
@@ -21,11 +20,8 @@ fun List<Word>.countPercentage(): Int {
     val totalIncludedCount = this.filter {
         it.wordStatus == WORD_LEARNED || it.wordStatus == WORD_ACTIVE
     }.size
-    Log.d("TAG_PERCENT", "countPercentage: learnedCount = $learnedCount")
-    Log.d("TAG_PERCENT", "countPercentage: totalIncludedCount = $totalIncludedCount")
     val lch = learnedCount * 100.0f
     val percentage = if (!(lch / totalIncludedCount).isNaN()) (lch / totalIncludedCount).roundToInt() else 0
-    Log.d("TAG_PERCENT", "countPercentage: $percentage%")
     return percentage
 }
 
